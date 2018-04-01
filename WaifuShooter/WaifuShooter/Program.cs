d0a02653c822dd4323d55de8bbf93ab5ac309074
SirMonoVonScopie <32022433+SirMonoVonScopie@users.noreply.github.com>
2018-04-01 16:20:30 +0200

    Projekt Erstellt.

---
+++ WaifuShooter/WaifuShooter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaifuShooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mono ist abgehoben aber seksi!");
+            while (true) { }
+        }
+    }
+}
